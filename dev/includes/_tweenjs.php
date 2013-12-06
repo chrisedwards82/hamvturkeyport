437e1176e6c04cd22f7283d9548b3a01eecced51
chrisedwardsmgb <chris.edwards@mcgarrybowen.com>
2013-12-06 17:09:04 -0500

    added tweenjs to includes

---
+++ dev/includes/_tweenjs.php
@@ -0,0 +1,1 @@
+<script src="http://code.createjs.com/tweenjs-0.5.0.min.js"></script>
--- dev/index.php
+++ dev/index.php
@@ -32,6 +32,7 @@
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
         <script src="js/plugins.js"></script>
 		<?php include 'includes/_easeljs.php' ?>
+		<?php include 'includes/_tweenjs.php' ?>
 		<?php include 'includes/_preloadjs.php' ?>
 		<?php include 'includes/_soundjs.php' ?>
 		<?php include 'includes/_gamejs.php' ?>
